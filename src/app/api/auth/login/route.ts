6419c441fb71c95e046d8fb349d716d15afa322c
Zaid Akbar <iamzaidakbar@gmail.com>
2024-12-05 21:04:38 +0530

    type check error fix

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { prisma } from "@/lib/prisma";
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined");
--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined");
